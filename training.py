f53ab28b6c7a2b2a8233fd6798ae43b0c749dac3
Kalyan <kalyanthiriveedhi2000@gmail.com>
2021-12-31 17:07:21 +0530

    updated training.py again

--- training.py
+++ training.py
@@ -1,167 +1,5 @@
-'''
-import json 
 
-import numpy as np 
-
-import tensorflow as tf 
-
-from tensorflow import keras
-
-from tensorflow.keras.models import Sequential
-
-from tensorflow.keras.layers import Dense, Embedding, GlobalAveragePooling1D
-
-from tensorflow.keras.preprocessing.text import Tokenizer
-
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-
-from sklearn.preprocessing import LabelEncoder
-
-"""
-
-    define few simple intents and bunch of messages 
-
-    that corresponds to those intents and
-
-    also map some responses according to each intent category.
-    
-
-    create a JSON file named “intents.json”
-
-
-"""
-
-
-data = json.load(open('intents.json','r',encoding="utf8"))
-    
-
-training_sentences = []
-
-training_labels = []
-
-labels = []
-
-responses = []
-
-
-"""
-
-The variable “training_sentences” holds all the training data (which are the sample messages in each intent category) 
-
-and the “training_labels” variable holds all the target labels correspond to each training data.
-
-Then we use “LabelEncoder()” function provided by scikit-learn to convert the target labels into a model understandable form.
-
-"""
-
-
-for intent in data['intents']:
-
-    for pattern in intent['patterns']:
-        training_sentences.append(pattern)
-
-        training_labels.append(intent['tag'])
-
-    responses.append(intent['responses'])
-    
-
-    if intent['tag'] not in labels:
-
-        labels.append(intent['tag'])
-        
-num_classes = len(labels)
-
-
-
-lbl_encoder = LabelEncoder()
-lbl_encoder.fit(training_labels)
-training_labels = lbl_encoder.transform(training_labels)
-
-
-"""
-
-Next, we vectorize our text data corpus by using the “Tokenizer” class and it allows us to limit our vocabulary size
-
-up to some defined number. When we use this class for the text pre-processing task,
-
-by default all punctuations will be removed, turning the texts into space-separated sequences of words,
-
-and these sequences are then split into lists of tokens.
-
-They will then be indexed or vectorized. We can also add “oov_token” which is a value for
-
-“out of token” to deal with out of vocabulary words(tokens) at inference time.
-
-"""
-
-vocab_size = 1000
-
-embedding_dim = 16
-
-max_len = 20
-
-oov_token = "<OOV>"
-
-tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_token)
-tokenizer.fit_on_texts(training_sentences)
-word_index = tokenizer.word_index
-sequences = tokenizer.texts_to_sequences(training_sentences)
-padded_sequences = pad_sequences(sequences, truncating='post', maxlen=max_len)
-
-
-#The “pad_sequences” method is used to make all the training text sequences into the same size.
-
-
-#model training
-
-model = Sequential()  #sequential model class of keras
-
-model.add(Embedding(vocab_size, embedding_dim, input_length=max_len))
-
-model.add(GlobalAveragePooling1D())
-
-model.add(Dense(16, activation='relu'))
-
-model.add(Dense(16, activation='relu'))
-
-model.add(Dense(num_classes, activation='softmax'))
-
-
-model.compile(loss='sparse_categorical_crossentropy', 
-
-              optimizer='adam', metrics=['accuracy'])
-
-
-#model.summary()
-
-
-#training the method by calling the fit method
-
-#epochs = 500
-
-#history = model.fit(padded_sequences, np.array(training_labels), epochs=epochs)
-
-
-model.save("chat_model")
-
-import pickle
-
-
-# to save the fitted tokenizer
-
-with open('tokenizer.pickle', 'wb') as handle:
-    pickle.dump(Tokenizer(num_words=vocab_size, oov_token=oov_token), handle, protocol=pickle.HIGHEST_PROTOCOL)
-    
-
-# to save the fitted label encoder
-
-with open('label_encoder.pickle', 'wb') as ecn_file:
-
-    pickle.dump(lbl_encoder, ecn_file, protocol=pickle.HIGHEST_PROTOCOL)
-'''
 import nltk
-nltk.download('punkt')
-nltk.download('wordnet')
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 import tensorflow as tf
@@ -202,9 +40,9 @@ classes = sorted(list(set(classes)))
 # documents = combination between patterns and intents
 print (len(documents), "documents")
 # classes = intents
-print (len(classes), "classes", classes)
+print (len(classes), "classes")
 # words = all words, vocabulary
-print (len(words), "unique lemmatized words", words)
+print (len(words), "unique lemmatized words")
 
 
 pickle.dump(words,open('E:\Python projects\chatbot\words.pkl','wb'))
@@ -252,30 +90,9 @@ model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(y_train[0]), activation='sigmoid'))
 
-#train_x=tf.ragged.constant(test_x)  #non rectangular python sequence to tensor conversion
-#train_y=tf.ragged.constant(train_y) #non rectangular python sequence to tensor conversion
-#test_x=tf.ragged.constant(test_x)   #non rectangular python sequence to tensor conversion
-#test_y=tf.ragged.constant(test_y)   #non rectangular python sequence to tensor conversion
-
-#train_data = tf.data.Dataset.from_tensor_slices(train_x, train_y)
-#print(train_data)
-#valid_data = tf.data.Dataset.from_tensor_slices(test_x, test_y)
-#print(valid_data)
-
-#ragged_tensor.RaggedTensor.__truediv__ = math_ops.truediv
-#ragged_tensor.RaggedTensor.__rtruediv__ = _right(math_ops.truediv)
-
-
-# Dummy methods
-#def _dummy_bool(_):
-"""Dummy method to prevent a RaggedTensor from being used as a Python bool."""
- # raise TypeError("RaggedTensor may not be used as a boolean.")
-
 x_train = np.asarray(x_train).astype(np.float32)
 y_train = np.asarray(y_train).astype(np.float32)
 
-#ragged_tensor.RaggedTensor.__bool__ = _dummy_bool
-#ragged_tensor.RaggedTensor.__nonzero__ = _dummy_bool
 
 # Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
 sgd = SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
@@ -284,6 +101,7 @@ model.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['accuracy'])
 #fitting and saving the model 
 hist = model.fit(x_train,y_train, epochs=200, batch_size=5, verbose=1)
 #hist = model.fit(train_x, train_y, epochs=200,validation_data=valid_data,verbose=1,batch_size=5)
+
 model.save('chatbot_model.h5', hist)
 
 print("model created")
